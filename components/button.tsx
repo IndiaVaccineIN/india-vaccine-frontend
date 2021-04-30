ab7ab1ece0b65552bd2dd1905045c237d677ff09
purvitrivedi <purvitriv@gmail.com>
2021-04-30 23:32:29 +0100

    Add styling for mobile hamburher components

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import { ReactNode } from "react";
+import Image from 'next/image'
+import { ReactNode } from 'react'
 
-import styles from "../styles/button.module.css"
+import styles from '../styles/button.module.css'
 
 interface ButtonInterface {
     children: ReactNode,
@@ -9,31 +9,31 @@ interface ButtonInterface {
 }
 
 export default function Button({ children, type }: ButtonInterface) {
-    /**
+  /**
      * Filter out null strings
      * 
      * @reference https://stackoverflow.com/a/41346932/12241836
      */
-    const classNames = [styles.button, type === "outline" ? styles.button_outline : styles.button_solid].filter(x => !!x).reduce((a, b) => a + ' ' + b)
-    return (
-        <div className={classNames}>
-            {
-            type === "solid" ?
-                <div className={styles.button_avatar}>
-                    <Image src={"/assets/small_human.svg"} height={25.22} width={20.66} />
-                </div> 
-            : null
-            }
+  const classNames = [styles.button, type === 'outline' ? styles.button_outline : styles.button_solid].filter(x => !!x).reduce((a, b) => a + ' ' + b)
+  return (
+    <div className={classNames}>
+      {
+        type === 'solid' ?
+          <div className={styles.button_avatar}>
+            <Image src={'/assets/small_human.svg'} height={25.22} width={20.66} />
+          </div> 
+          : null
+      }
             
-            { children}
+      { children}
 
-            {
-            type === "outline" ?
-                <div className={styles.button_avatar}>
-                    <Image src={"/assets/arrow-right-circle.svg"} height={24} width={24} />
-                </div>
-            : null
-            }
-        </div>
-    )
+      {
+        type === 'outline' ?
+          <div className={styles.button_avatar}>
+            <Image src={'/assets/arrow-right-circle.svg'} height={24} width={24} />
+          </div>
+          : null
+      }
+    </div>
+  )
 }
