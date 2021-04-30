9382f43ee7fc8305a2569424ae174f2ee44ef4cf
purvitrivedi <purvitriv@gmail.com>
2021-05-01 00:13:28 +0100

    Conditionally render menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 import LanguageSwitcher from "./language_switcher";
 import styles from '../styles/navbar.module.css'
@@ -9,6 +10,7 @@ export default function Navbar() {
   /**
    * @reference https://gist.github.com/remy/0dde38897d6d660f0b63867c2344fb59#gistcomment-3714984
    */
+  const [isMenuOpen, setMenu] = useState(false)
   const router = useRouter()
   const activeLink = (path, content, activeClass = styles.active_route, normalClass = '') => {
     const className = router.pathname === path ? activeClass : normalClass
@@ -17,20 +19,25 @@ export default function Navbar() {
   return (
     <>
     <nav className={styles.navbar_wrapper}>
-      <div className={`${styles.navbar_content} ${styles.navbar_content_mobile}`}>
-        <div className={styles.logo_wrapper}>
+      <div className={`${styles.navbar_content} ${isMenuOpen ? styles.navbar_content_mobile : styles.navbar_content}`}>
+        <div className={`${styles.logo_wrapper} ${styles.logo_wrapper_mobile} `}>
           <Image src={'/assets/logo.svg'} height={200} width={200}></Image>
-          <button className={styles.cross}>
-            <div className={`${styles.cross_line} ${styles.cross_1}`}></div>
-            <div className={`${styles.cross_line} ${styles.cross_2}`}></div>
-          </button>
+          {isMenuOpen && 
+            <button className={styles.cross} onClick={()=> setMenu(false)}>
+              <div className={`${styles.cross_line} ${styles.cross_1}`}></div>
+              <div className={`${styles.cross_line} ${styles.cross_2}`}></div>
+            </button>
+          }
         </div>
-        <button className={styles.hamburger}>
-          <div className={styles.hamburger_line}></div>
-          <div className={styles.hamburger_line}></div>
-          <div className={styles.hamburger_line}></div>
-        </button>
-        <div className={`${styles.routes} ${styles.routes_mobile}`}>
+
+        {!isMenuOpen && 
+            <button className={styles.hamburger} onClick={()=> setMenu(true)}>
+              <div className={styles.hamburger_line}></div>
+              <div className={styles.hamburger_line}></div>
+              <div className={styles.hamburger_line}></div>
+            </button>
+        }
+        <div className={`${styles.routes} ${isMenuOpen ? '' : styles.routes_mobile}`}>
           {activeLink('/', 'About')}
           <a
             href={'https://forms.gle/HeH3xrvjP1VfFUzM7'}
