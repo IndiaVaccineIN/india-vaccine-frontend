ce4427d0b79a5036d466715fb9556967204040f4
Harsha <emails.harsha@gmail.com>
2021-05-02 01:54:53 +0530

    Added a search bar and check availability page elements

--- pages/check_availability.tsx
+++ pages/check_availability.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/navbar";
 
 import styles from '../styles/check_availability.module.css'
 
+
 export default function CheckAvailability() {
     return (
         <div>
@@ -14,8 +15,16 @@ export default function CheckAvailability() {
             </Head>
             <Navbar />
             <div className={styles.container}>
+                <h1>Find a COVID-19 vaccine for yourself or a loved one</h1>
+                <div className="flex">
+                <label className={styles.label}>
+                <input type="text" className={styles.searchBar} placeholder="Enter your Pincode or Area" /></label>
+                    <button type="submit" className={styles.searchButton}>Find Vaccine</button></div>
+                <div className="flex">
+                    <img src="/assets/avatar_landing.svg" alt="Illustration" />
+                    <span>Everyone over 18 is now eligible for free, safe, and reliable COVID-19 vaccines in India. But it can be difficult to find out where and how to get a shot. Enter your zip code or area to find confirmed vaccination locations near you, and learn how to make an appointment.</span>
+                </div>
             <main className={styles.main}>
-                Hello
             </main>
 
             </div>
