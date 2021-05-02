c92a40bcfb64a6231c5fa6a9ad8c51cc6758382a
Preet Parekh <preetjdpdev@gmail.com>
2021-05-02 14:45:06 +0530

    🚧 add types

--- components/cvc_card.tsx
+++ components/cvc_card.tsx
@@ -1,6 +1,7 @@
+import { components } from "../api/interfaces"
 import styles from "../styles/cvc_card.module.css"
 
-export default function CvcCard() {
+export default function CvcCard(props: components["schemas"]["CVCResponseData"]) {
     return (
         // <>
             <div className={styles.cvc_card}>
