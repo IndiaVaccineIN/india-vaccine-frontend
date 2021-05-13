d629e0e1d548f0b2fcf41425c780cf7fafa151f0
Savitha Gollamudi <savitha.gollamudi@gmail.com>
2021-05-13 16:52:16 +0530

    Added telugu locale

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     // locales: ['en-us', 'ta-in', 'hi-in', 'kn-in'],
-    locales: ["en-us", "hi-in", "gu-in"],
+    locales: ["en-us", "hi-in", "gu-in", "te-in"],
     defaultLocale: "en-us",
   },
 };
--- shared/LocalesMap.ts
+++ shared/LocalesMap.ts
@@ -2,4 +2,5 @@ export const LocalesMap = {
   "en-us": "English",
   "hi-in": "हिंदी",
   "gu-in": "ગુજરાતી",
+  "te-in": "తెలుగు",
 };
