6431f122098c92642488f0d1364505b5c5ba82c3
Shivam Singhal <kas442000@gmail.com>
2021-05-18 23:18:09 +0530

    add ms clarity script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,13 @@ export default class MyDocument extends Document {
             gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
+
+
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "6qvs4lw55s");
           `,
             }}
           />
